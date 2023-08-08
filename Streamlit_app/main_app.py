f01916c534fc19733e73d0e6b30c8c0100b317e9
Vidhan Mehta <vidhanmehta3@gmail.com>
2023-08-08 23:57:37 +0530

    changes in streamlit app
    added symptoms and how to cure the disease.

--- Streamlit_app/main_app.py
+++ Streamlit_app/main_app.py
@@ -26,5 +26,32 @@ if submit:
 		opencv_image = cv2.resize (opencv_image, (256,256))
 		opencv_image.shape=(-1,256,256,3)
 		Y_pred=model.predict (opencv_image)
+
 		result=CLASS_NAMES [np.argmax (Y_pred)]
+		y_pred_onehot=tf.keras.utils.to_categorical(Y_pred)
+		result=CLASS_NAMES [np.argmax (y_pred_onehot)]
+		
+
+		
+
 		st.title (str("This is Maize leaf with "+ result.split('-') [1]))
+		if result is 'Corn-Common_Rust':
+			st.title(str("Causes for "+result))
+			st.write(str("Common rust in maize is caused by a fungal pathogen 				      called Puccinia sorghi. This disease occurs when 					      favorable environmental conditions and susceptible 				      host plants come together. Additional reasons 					      maybe Presence of Fungal Spores, Lack of crop 					      rotation")) 
+			st.title(str("Remedies for "+result))
+			st.write(str("Use Fertilizers such as Nitrogen, Phosphorus, 					      Potassium, Organic Fertilizers like Compost, 					      manure "))
+			st.write(str(" also Monitor Regularly-Early detection 						      allows for prompt action"))
+			st.write(str("Nutrient Management-								      Properly nourished plants are more resilient to 					      diseases."))
+			st.write(str("Consult Experts-in case of likelihood of 						      severe infections."))
+
+		if result is 'Corn-Healthy':
+			st.title(str("To Maintain "+result))
+			st.write(str("To maintain the good health of your crop and 					      increase the yield follow Good Agricultural        				      Practices (GAPs) ")) 
+			st.write(str("Fertilization and organic manure: Provide balanced 				      and appropriate fertilization based on soil test 					      results."))
+			st.write(str("Irrigation Management: Water the maize crop 					      consistently and avoid both overwatering and    					      underwatering."))
+			st.write(str("Crop Rotation: Practice crop rotation to break 					     disease cycles and prevent the buildup of pathogens  				     in the soil."))
+			st.write(str("Nutrient Management-								      Properly nourished plants are more resilient to 					      diseases."))
+			
+
+
+				      
